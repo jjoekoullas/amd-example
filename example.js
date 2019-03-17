3728a7c06cc0dc2e685539ca1098dac61bac4138
J. Joe Douglas <jjoedouglas@gmail.com>
2019-03-17 14:47:03 -0400

    es6

--- example.js
+++ example.js
@@ -1,13 +1,2 @@
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-define(["require", "exports", "lodash"], function (require, exports, _) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    _ = __importStar(_);
-    exports.val = _.get({ something: 42 }, 'something');
-});
+import * as _ from 'lodash';
+export var val = _.get({ something: 42 }, 'something');
